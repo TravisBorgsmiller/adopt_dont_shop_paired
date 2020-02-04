0e258a906a6250bd114377e045b1adba0e6df797
Jordan <jordan.wallace.williams@gmail.com>
2020-02-04 14:52:20 -0700

    Update edit shelter spec to cover all of user stories 5 and 6

--- spec/features/reviews/user_can_edit_shelter_reviews_spec.rb
+++ spec/features/reviews/user_can_edit_shelter_reviews_spec.rb
@@ -1,54 +1,67 @@
 require 'rails_helper'
 
-RSpec.describe 'As a visitor', type: :feature do
-  describe 'When I visit a shelter show page and click edit review' do
-    it 'it loads a form to update the review' do
-      shelter1 = Shelter.create(
-        name: "Mike's Shelter",
-        address: '1331 17th Street',
-        city: 'Denver',
-        state: 'CO',
-        zip: '80202'
-      )
+RSpec.describe 'As a visitor I can edit a shelter review from its show page', type: :feature do
+  before :each do
+    shelter1 = Shelter.create(
+      name: "Mike's Shelter",
+      address: '1331 17th Street',
+      city: 'Denver',
+      state: 'CO',
+      zip: '80202'
+    )
 
-      pet1 = shelter1.pets.create(
-        image: 'https://cdn2-www.dogtime.com/assets/uploads/2011/03/puppy-development.jpg',
-        name: 'Athena',
-        description: 'butthead',
-        age: '1',
-        sex: 'female',
-        status: 'adoptable'
-      )
+    pet1 = shelter1.pets.create(
+      image: 'https://cdn2-www.dogtime.com/assets/uploads/2011/03/puppy-development.jpg',
+      name: 'Athena',
+      description: 'butthead',
+      age: '1',
+      sex: 'female',
+      status: 'adoptable'
+    )
 
-      review1 = shelter1.reviews.create(
-        title: "A glowing review",
-        rating: "5",
-        content: "Great facility, friendly staff!",
-        image: "https://i.imgur.com/dciDr8Q.jpg"
-      )
+    review1 = shelter1.reviews.create(
+      title: "A glowing review",
+      rating: "5",
+      content: "Great facility, friendly staff!",
+      image: "https://i.imgur.com/dciDr8Q.jpg"
+    )
+  end
 
-      visit "/shelters/#{shelter1.id}"
-      expect(page).to have_content('A glowing review')
-      expect(page).to have_content('Rating: 5')
-      expect(page).to have_content('Great facility, friendly staff!')
-      expect(page).to have_css("img[src*='#{review1.image}']")
-      click_link 'Update Review'
+  it 'it links to a form to update the review' do
+    visit "/shelters/#{shelter1.id}"
 
-      expect(current_path).to eq("/shelters/#{shelter1.id}/edit")
+    expect(page).to have_content('A glowing review')
+    expect(page).to have_content('Rating: 5')
+    expect(page).to have_content('Great facility, friendly staff!')
+    expect(page).to have_css("img[src*='#{review1.image}']")
 
-      fill_in 'Title', with: 'Another glowing review'
-      fill_in 'Rating', with: '4'
-      fill_in 'Content', with: 'Super dope'
-      fill_in 'Image', with: 'https://i.imgur.com/dciDr8Q.jpg'
-      click_button 'Save'
+    click_link 'Update Review'
 
-      expect(current_path).to eq("/shelters/#{shelter1.id}")
+    expect(current_path).to eq("/shelters/#{shelter1.id}/reviews/edit")
+  end
 
-      expect(page).to have_content('Another glowing review')
-      expect(page).to have_content('Rating: 4')
-      expect(page).to have_content('Super dope')
-      expect(page).to have_css("img[src*='#{review1.image}']")
-    end
+  it 'does not allow a user to update a review without required information' do
+    visit "/shelters/#{shelter1.id}/reviews/edit"
+
+    fill_in 'Title', with: 'Another glowing review'
+    fill_in 'Image', with: 'https://i.imgur.com/dciDr8Q.jpg'
+    click_button 'Save'
+
+    expect(page).to have_content('Review not updated. Please complete required fields.')
+    expect(current_path).to eq("/shelters/#{shelter1.id}/reviews/edit")
+    expect(page).to have_button('Save')
+
+    fill_in 'Title', with: 'Another glowing review'
+    fill_in 'Rating', with: '4'
+    fill_in 'Content', with: 'Super dope'
+    fill_in 'Image', with: 'https://i.imgur.com/dciDr8Q.jpg'
+    click_button 'Save'
+
+    expect(current_path).to eq("/shelters/#{shelter1.id}")
+    expect(page).to have_content('Another glowing review')
+    expect(page).to have_content('Rating: 4')
+    expect(page).to have_content('Super dope')
+    expect(page).to have_css("img[src*='#{review1.image}']")
   end
 end
 
