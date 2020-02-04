54027c12295de40ebefaeac349d177955b9da460
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-04 01:42:20 -0700

    spec for user story 4

--- spec/features/reviews/user_can_create_new_review_spec.rb
+++ spec/features/reviews/user_can_create_new_review_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'As a visitor I can create a new shelter review' do
-  it 'links form from shelter show page' do
-    shelter1 = Shelter.create(
+  before :each do
+    @shelter1 = Shelter.create(
       name: "Mike's Shelter",
       address: '1331 17th Street',
       city: 'Denver',
@@ -10,7 +10,7 @@ RSpec.describe 'As a visitor I can create a new shelter review' do
       zip: '80202'
     )
 
-    pet1 = shelter1.pets.create(
+    @pet1 = @shelter1.pets.create(
       image: 'https://cdn2-www.dogtime.com/assets/uploads/2011/03/puppy-development.jpg',
       name: 'Athena',
       description: 'butthead',
@@ -18,12 +18,14 @@ RSpec.describe 'As a visitor I can create a new shelter review' do
       sex: 'female',
       status: 'adoptable'
     )
+end
+  it 'links form from shelter show page' do
 
-    visit "/shelters/#{shelter1.id}"
-    expect(current_path).to eq("/shelters/#{shelter1.id}")
+    visit "/shelters/#{@shelter1.id}"
+    expect(current_path).to eq("/shelters/#{@shelter1.id}")
 
-    click_link "Create new review for #{shelter1.name}"
-    expect(current_path).to eq("/shelters/#{shelter1.id}/reviews/new")
+    click_link "Create new review for #{@shelter1.name}"
+    expect(current_path).to eq("/shelters/#{@shelter1.id}/reviews/new")
 
     fill_in :title, with: 'Bomb Review'
     select '5', from: :rating
@@ -31,8 +33,25 @@ RSpec.describe 'As a visitor I can create a new shelter review' do
     fill_in :image, with: ''
     click_button 'Submit'
 
-    expect(current_path).to eq("/shelters/#{shelter1.id}")
+    expect(current_path).to eq("/shelters/#{@shelter1.id}")
     expect(page).to have_content('Bomb Review')
     expect(page).to have_content('Fabulous facilities')
   end
+  it "does not allow a user to create a review without required information" do
+
+   visit "/shelters/#{@shelter1.id}/reviews/new"
+
+   click_button('Submit')
+
+   expect(page).to have_content('Review not created. Please complete required fields.')
+   expect(page).to have_button('Submit')
+
+   fill_in 'title', with: 'This place is remarkable!'
+   fill_in 'content', with: 'Friendly attendants.'
+
+   click_button('Submit')
+
+   expect(page).to have_content('Review not created. Please complete required fields.')
+   expect(page).to have_button('Submit')
+ end
 end
