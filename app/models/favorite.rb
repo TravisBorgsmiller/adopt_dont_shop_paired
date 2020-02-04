78ae7ec917e8aa03840bcd0befdcc2c6022aacc7
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-04 02:49:19 -0700

    add favorite poro

---
+++ app/models/favorite.rb
@@ -0,0 +1,15 @@
+class Favorite
+  attr_reader :contents
+
+  def initialize(initial_contents)
+    @contents = initial_contents || Hash.new(0)
+  end
+
+  def total_count
+    @contents.count
+  end
+
+  def add_favorite_pet(id)
+    @contents[id.to_s] = 1
+  end
+end
