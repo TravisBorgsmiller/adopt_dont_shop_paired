bb51df7bba586f48396a290ec0923205b4f74bd9
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-05 15:50:32 -0700

    add favorite helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  before_action :favorite
+protect_from_forgery with: :exception
+helper_method :favorite
+before_action :favorite
 
   def favorite
     @favorites ||= Favorite.new(session[:favorites])
