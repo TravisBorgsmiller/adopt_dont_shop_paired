f6d84bbb8cacfa0068d102234d3822ab6f379248
Jordan <jordan.wallace.williams@gmail.com>
2020-02-05 21:54:27 -0700

    Add find_pets method to application model to use with application_controller new action

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationsController < ApplicationController
   def new
     binding.pry
+    @pets = Application.find_pets(@favorites.contents)
   end
 end
--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,13 @@ class Application < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip, :phone, :description
   has_many :pet_applications
   has_many :pets, through: :pet_applications
+
+  def self.find_pets(favorites)
+    keys = favorites.keys
+    @favorite_pets = []
+    keys.each do |key|
+      @favorite_pets << Pet.find(key)
+    end
+    @favorite_pets
+  end
 end
