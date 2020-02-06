c51c584e43c4720b87b980c72e95c786d2f35b28
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-06 15:17:22 -0700

    switch logic from destroy to update for decrementing favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,21 +9,22 @@ class FavoritesController < ApplicationController
 
   def update
     pet = Pet.find(params[:id])
-    @favorites.add_favorite_pet(pet.id)
-    session[:favorites] = @favorites.contents
-    flash[:success] = "#{pet.name} has been added to your favorites!"
-    redirect_to "/pets/#{pet.id}"
+    if params[:delete_or_favorite] == "favorite"
+      @favorites.add_favorite_pet(pet.id)
+      session[:favorites] = @favorites.contents
+      flash[:success] = "#{pet.name} has been added to your favorites!"
+      redirect_to "/pets/#{pet.id}"
+    else
+      @favorites.remove_pet(pet.id)
+      session[:favorites] = @favorites.contents
+      flash[:success] = "#{pet.name} has been removed from Favorites!"
+      redirect_to "/favorites"
+    end
   end
 
   def destroy
-    if params[:id]
-      pet = Pet.find(params[:id])
-      favorite.remove_pet(pet.id)
-      flash[:notice] = "#{pet.name} has been removed from Favorites!"
-    else
-      favorite.remove_all
-      flash[:notice] = "Your favorited pets have been removed"
-   end
+    favorite.remove_all
+    flash[:notice] = "Your favorited pets have been removed"
     redirect_to "/favorites"
     session[:favorites] = favorite.contents
   end
