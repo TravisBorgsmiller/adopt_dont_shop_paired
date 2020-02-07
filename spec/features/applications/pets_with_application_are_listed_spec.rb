78c089d37e28af537b6aeb9875cc92360c66ca17
Jordan <jordan.wallace.williams@gmail.com>
2020-02-06 21:04:15 -0700

    Add tests for user story 18: favorites index lists pets with pending applications

---
+++ spec/features/applications/pets_with_application_are_listed_spec.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'As a vistor after I apply to adopt pets', type: :feature do
+  before :each do
+    @shelter1 = Shelter.create(
+      name: "Mike's Shelter",
+      address: '1331 17th Street',
+      city: 'Denver',
+      state: 'CO',
+      zip: '80202'
+    )
+
+    @pet1 = @shelter1.pets.create(
+      image: 'https://cdn2-www.dogtime.com/assets/uploads/2011/03/puppy-development.jpg',
+      name: 'Athena',
+      description: 'butthead',
+      age: '1',
+      sex: 'female',
+      status: 'adoptable'
+    )
+
+    @pet2 = @shelter1.pets.create(
+      image: 'https://s.abcnews.com/images/Lifestyle/puppy-ht-3-er-170907_16x9_992.jpg',
+      name: 'Odell',
+      description: 'good dog',
+      age: '4',
+      sex: 'male',
+      status: 'adoptable'
+    )
+
+    @review1 = @shelter1.reviews.create(
+      title: 'A glowing review',
+      rating: '5',
+      content: 'Great facility, friendly staff!',
+      image: 'https://i.imgur.com/dciDr8Q.jpg'
+    )
+
+    visit "/pets/#{@pet1.id}"
+    click_button 'Favorite'
+
+    visit "/pets/#{@pet2.id}"
+    click_button 'Favorite'
+
+    visit '/favorites'
+    click_link 'Adopt Pet(s)'
+
+    expect(current_path).to eq('/applications/new')
+
+    within("div#pet_#{@pet1.id}") do
+      check :adopt_pets_
+    end
+
+    within("div#pet_#{@pet2.id}") do
+      check :adopt_pets_
+    end
+
+    fill_in 'Name', with: 'Jordan'
+    fill_in 'Address', with: '4231 Ponderosa Court'
+    fill_in 'City', with: 'Boulder'
+    fill_in 'State', with: 'CO'
+    fill_in 'Zip', with: '80301'
+    fill_in 'Phone', with: '323.940.3227'
+    fill_in 'Description', with: "I'm a puppy parent"
+    click_button 'Submit'
+
+    expect(current_path).to eq('/favorites')
+  end
+
+  it 'they are listed on the favorites index' do
+    visit '/favorites'
+
+    within('div.petsWithApplications') do
+      expect(page).to have_content('Athena')
+      expect(page).to have_link('Athena')
+      expect(page).to have_content('Odell')
+      expect(page).to have_link('Odell')
+
+      click_link 'Odell'
+    end
+
+    expect(current_path).to eq("/pets/#{@pet2.id}")
+
+    visit '/favorites'
+
+    within('div.petsWithApplications') do
+      expect(page).to have_content('Athena')
+      expect(page).to have_link('Athena')
+      expect(page).to have_content('Odell')
+      expect(page).to have_link('Odell')
+
+      click_link 'Athena'
+    end
+
+    expect(current_path).to eq("/pets/#{@pet1.id}")
+  end
+end
