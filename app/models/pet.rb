dada3806debc8fc718237cbd525f6292822005a1
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-09 07:50:39 -0700

    more readable pet model validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,11 @@
 class Pet < ApplicationRecord
-  validates_presence_of :name, :image, :age, :sex, :shelter_id, :status, :description
+  validates_presence_of :name,
+                        :image,
+                        :age,
+                        :sex,
+                        :shelter_id,
+                        :status,
+                        :description
   belongs_to :shelter
   has_many :pet_applications
   has_many :applications, through: :pet_applications
