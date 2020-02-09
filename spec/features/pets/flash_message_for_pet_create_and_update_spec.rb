a09e6440a548f4c1f9275dc45ff33d2dec61bc1e
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-02-09 07:50:23 -0700

    add user story 33 test

---
+++ spec/features/pets/flash_message_for_pet_create_and_update_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor when I create or update a pet with incomplete info' do
+  it 'displays flash message with fields that are incomplete' do
+    shelter3 = Shelter.create!(
+      name: 'Zeus\'s Puppy Pals',
+      address: '3734 Cedar Court',
+      city: 'Boulder',
+      state: 'CO',
+      zip: '80301')
+    buster = shelter3.pets.create!(
+      image: 'https://image.shutterstock.com/image-photo/brindled-plott-hound-puppy-on-600w-79691980.jpg',
+      name: 'Buster',
+      description: 'Friendly',
+      age: 3,
+      sex: 'male')
+
+
+    visit "/shelters/#{shelter3.id}/pets"
+
+    click_link 'Create Pet'
+
+    expect(current_path).to eq("/shelters/#{shelter3.id}/pets/new")
+
+    fill_in 'Image', with: 'https://image.shutterstock.com/image-photo/brindled-plott-hound-puppy-on-600w-79691980.jpg'
+    fill_in 'Name', with: 'Zeus'
+    fill_in 'Description', with: 'wild'
+    fill_in 'Age', with: ''
+    fill_in 'Sex', with: 'male'
+    click_button 'Create Pet'
+    expect(page).to have_content("Age can't be blank")
+    expect(page).to have_button('Create Pet')
+
+    visit "/pets/#{buster.id}"
+    click_link 'Update Pet'
+    fill_in 'Sex', with: ''
+    fill_in 'Description', with: ''
+    click_button 'Update Pet'
+    expect(page).to have_content("Sex can't be blank and Description can't be blank")
+    expect(page).to have_button('Update Pet')
+  end
+end
