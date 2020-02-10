eaf44da35fbd6d23b8596f518cdf39881b5aa80d
Jordan <jordan.wallace.williams@gmail.com>
2020-02-09 20:18:33 -0700

    Add average_rating and applications_count methods

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,4 +2,31 @@ class Shelter < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   has_many :pets, dependent: :destroy
   has_many :reviews, dependent: :destroy
+
+  def pet_count
+    pets.count
+  end
+
+  def average_rating
+    if reviews.empty? == true
+      'No ratings yet.'
+    else
+      ratings = reviews.map do |review|
+        review.rating.to_i
+      end
+      ratings.sum / ratings.size
+    end
+  end
+
+  def applications_count
+    apps = []
+    pets.each do |pet|
+      if pet.applications.empty?
+        next
+      else
+        apps << pet.applications
+      end
+    end
+    apps.size
+  end
 end
