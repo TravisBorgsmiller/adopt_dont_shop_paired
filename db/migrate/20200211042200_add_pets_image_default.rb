1ba124562e2d283e505e8ba6170d283dbc97c6fc
Jordan <jordan.wallace.williams@gmail.com>
2020-02-11 01:23:46 -0700

    Add migration to add defaul image to pets table

--- db/migrate/20200211042200_add_pets_image_default.rb
+++ db/migrate/20200211042200_add_pets_image_default.rb
@@ -1,5 +1,6 @@
 class AddPetsImageDefault < ActiveRecord::Migration[5.1]
   def change
-    change_column :pets, :image, :string, default: 'https://i.imgur.com/HpDSNna.jpg'
+    change_column_default :pets, :image, :string, default: 'https://i.imgur.com/HpDSNna.jpg'
+    change_column_default :reviews, :image, :string, default: 'https://i.imgur.com/dciDr8Q.jpg'
   end
 end
